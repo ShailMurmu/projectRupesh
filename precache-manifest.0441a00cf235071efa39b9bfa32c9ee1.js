37848b9d87b304d7e5343936561828bec866ac9c
Shailendra Murmu <199089@tatasteel.com>
2020-03-06 11:21:46 +0530

    Updates

---
+++ precache-manifest.0441a00cf235071efa39b9bfa32c9ee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159a682f191a1e902bb034941ea9c92d",
+    "url": "/RupeshJapSite/index.html"
+  },
+  {
+    "revision": "08dc33ba54f3ed8dff41",
+    "url": "/RupeshJapSite/static/css/main.f4ec79a3.chunk.css"
+  },
+  {
+    "revision": "a56b133f01367e06066c",
+    "url": "/RupeshJapSite/static/js/2.c06183d9.chunk.js"
+  },
+  {
+    "revision": "41b0c3da6a8866c17968078c7c246d41",
+    "url": "/RupeshJapSite/static/js/2.c06183d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08dc33ba54f3ed8dff41",
+    "url": "/RupeshJapSite/static/js/main.2299f1ad.chunk.js"
+  },
+  {
+    "revision": "355a2059cc66fd85557f",
+    "url": "/RupeshJapSite/static/js/runtime-main.88dbe736.js"
+  }
+]);
